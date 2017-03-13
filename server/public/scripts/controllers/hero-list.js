368c28c8680f196e6338b8dffc9240820ca614e9
Catherine J <cjarocki@gmail.com>
2017-03-13 14:01:06 -0500

    no display

--- server/public/scripts/controllers/hero-list.js
+++ server/public/scripts/controllers/hero-list.js
@@ -1,3 +1,34 @@
-app.controller('HeroListController', function(){
+app.controller('HeroListController', ['$http', function($http){
     console.log('Hero List Controller loaded');
-});
+
+    var self = this;
+    self.newHero = {};
+    self.heroList = [];
+
+    getHeroes();
+
+    function getHeroes(){
+      $http.get('/heroes').then(function(response){
+        console.log('response: ', response.data);
+        self.heroList = response.data;
+      });
+    }
+
+    self.addHero = function(){
+      console.log(self.newHero);
+      $http.post('/heroes', self.newHero).then(function(response) {
+        self.newHero = {};
+        getHeroes();
+      });
+    }
+
+    self.deleteHero = function(){
+      console.log(self.newHero);
+      $http.delete('/heroes', self.newHero).then(function(response){
+        getHeroes();
+      })
+    }
+
+
+
+}]);
